bc2cbef6abc90801ca7ce671322339adeaaab69f
noelcv <79803356+noelcv@users.noreply.github.com>
2022-09-08 18:10:16 +0200

    fe: generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { HeaderComponent } from './header/header.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchbarComponent,
+    HeaderComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
