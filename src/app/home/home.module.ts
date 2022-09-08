17d92a3a66de9556a8c9d56861b3becf954cf40f
noelcv <79803356+noelcv@users.noreply.github.com>
2022-09-08 18:53:00 +0200

    layout: components architecture

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { maxHeaderSize } from 'http';
+import { HeaderComponent } from '../header/header.component';
+import { SearchbarComponent } from '../searchbar/searchbar.component';
+import { ResultsComponent } from '../results/results.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, SearchbarComponent, ResultsComponent],
   imports: [
     CommonModule
   ]
