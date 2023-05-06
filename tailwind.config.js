441fc9491bec3ccfd7a595841afa457e8056c8b3
noelcv <noelguiavieira@gmail.com>
2023-05-06 17:48:05 +0200

    WQFE-Upgrade-Angular-16

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,10 @@ require('dotenv').config();
 const purgeEnabled = process.env.PURGE_ENABLED || false;
 module.exports = {
   mode: 'jit',
-  purge: [],
-  purge: {
   content: [
     "./src/**/*.{html,ts}",
     "./src/**/*.css"
-  ]
-},
+  ],
   theme: {
     extend: {},
   },
